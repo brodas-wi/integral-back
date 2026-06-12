8240064f24589d9b918d95e50600edaf2317e5e7
Steve <brodas@web-informatica.com>
2026-06-12 10:56:56 -0600

    feat(scripts): remove script type icon from index list view
    Remove the JavaScript icon element from the script list items in the
    index view to simplify the UI layout and reduce visual clutter in the
    scripts listing page.

--- resources/views/scripts/index.blade.php
+++ resources/views/scripts/index.blade.php
@@ -60,11 +60,6 @@
                     <div class="border border-gray-200 rounded-lg p-4 hover:bg-gray-50 transition-colors script-item"
                         id="script-item-{{ $script->id }}">
                         <div class="flex items-start gap-4">
-
-                            <div class="shrink-0 w-10 h-10 rounded-lg flex items-center justify-center bg-yellow-100 text-yellow-700">
-                                <i class="ri-javascript-line text-xl"></i>
-                            </div>
-
                             <div class="flex-1 min-w-0">
                                 <div class="flex flex-wrap items-center gap-2 mb-1">
                                     @php
